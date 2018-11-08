3f917b05c7f7d79f873f61e8556127855ea38371
Dmytro Torianik <dmytro_torianik@epam.com>
2018-11-08 14:28:00 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from sanic import Sanic
+from jinja2 import Environment, PackageLoader
+from sanic.response import html
+import os
+
+app = Sanic()
+templates = Environment(loader=PackageLoader('app', 'templates'))
+
+@app.route('/')
+async def home_page(request):
+    template = templates.get('index.html')
+    html_content = template.render()
+    return html(html_content)
+
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8000)),
+        workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
+        debug=bool(os.environ.get('DEBUG', '')))
